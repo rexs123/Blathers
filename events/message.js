5b8540707c191934c42090f62d92947440afe8aa
Rama Patel <me@rexsdev.com>
2020-07-05 14:14:45 -0400

    I put the line in the code block like a bean.

--- events/message.js
+++ events/message.js
@@ -21,8 +21,8 @@ module.exports = async (client, message) => {
 			'q! # Event queue commands \n' +
 			'^  # Secondary music bot \n' +
 			'~  # Villager lookup \n' +
-			'To look up the list of commands, type {prefix} help in <#702716081755652136> \n' +
 			'``` \n' +
+			'To look up the list of commands, type `{prefix} help` in <#702716081755652136> \n' +
 			'If you need help or want to contact our staff for assistance, send a direct message to @Staff Support and we will get back to you as soon as we can.'
 		);
 	}
